315969817d7c57d930daaf5ebc8c67334b74bf22
luis <6010ldaniel@gmail.com>
2022-12-01 23:07:33 -0300

    Agenda.js

--- agenda/src/App.js
+++ agenda/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function agregar(){
+
+     const nomes = document.getElementById('nome')
+     const emails = document.getElementById('email')
+     const telefs = document.getElementById('telef')
+
+     const dados =[nomes.value, emails.value, telefs.value] 
+     console.log(nomes.value)
+
+     const novoElement = document.createElement('li')
+     novoElement.textContent = dados
+
+     const container = document.getElementById('container')
+     container.appendChild(novoElement)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div> Agenda </div>
+
+      <hr></hr>
+
+      <div>
+        <button type="submit" onClick={agregar}>Agregar</button>
+        <button>Atualizar</button>
+      </div>
+
+
+
+      <form>
+        <input id="nome"  placeholder='Nome'></input>
+        <input id="email" placeholder='Email'></input>
+        <input id="telef" placeholder='Telefone'></input>
+      </form>
+
+     
+
+      <ul id="container">
+
+      </ul>
+
+     
+      <button type="submit">Deletar</button>
+
+
     </div>
   );
 }
